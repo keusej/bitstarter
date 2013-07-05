b439ffc2d03d25d3ff34cc54e9bf9f607df5ca9b
Jordan <jordan@woelfware.com>
2013-07-05 12:42:54 -0500

    send back index.html data

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+// get the file buffer and convert to regular string
+var buffer = fs.readFileSync("index.html");
+var pagedata = buffer.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(pagedata);
 });
 
 var port = process.env.PORT || 5000;
